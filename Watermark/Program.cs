a7426443d053d97c08f1ff6cc834ea93bb13e3c3
igodsmark <igodsmark79@gmail.com>
2023-11-07 22:08:33 +0000

    Add project files.

---
+++ Watermark/Program.cs
@@ -0,0 +1,35 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Watermark;
+
+
+List<string> files = new List<string>();
+if(args.Length > 0)
+{
+    Console.WriteLine(args[0]);
+    var directories = Directory.GetDirectories(args[0]);
+    if(directories.Length > 0)
+    {
+        foreach(var directory in directories)
+        {
+            Console.WriteLine($"Searching for images in {directory}");
+            files.AddRange(Directory.GetFiles(directory));
+        }
+    }
+    else
+    {
+
+        files = Directory.GetFiles(args[0]).ToList();
+
+    }
+    
+}
+else
+{
+
+    files = Directory.GetFiles(System.IO.Directory.GetCurrentDirectory(), "*.jpg").ToList();
+}
+foreach(var file in files)
+{
+    Console.WriteLine(file);
+    Watermark.Utilities.WatermarkedImage(Directory.GetParent(file).FullName, Path.GetFileName(file), "Watermark!", Directory.GetParent(file).FullName);
+}
---
+++ Watermark/WatermarkedImage.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using static System.Net.Mime.MediaTypeNames;
+using Image = System.Drawing.Image;
+
+namespace Watermark
+{
+    internal static class Utilities
+    {
+        internal static void WatermarkedImage(string path, string fileName, string message, string outputFolder)
+        {
+            bool debug = false;
+            if(!Directory.Exists(outputFolder + @"\Watermarked"))
+            {
+                Directory.CreateDirectory(outputFolder + @"\Watermarked");
+            }
+            string outputPath = outputFolder + @"\Watermarked\";
+            using (Image image = Image.FromFile(path + "\\" + fileName))
+            {
+                Graphics graphics = Graphics.FromImage(image);
+                int fontSize = 25;
+                if(image.Width > 1500)
+                {
+                    fontSize = 85;
+                }
+                Font font = new Font("Arial", 80, FontStyle.Bold);
+                SolidBrush brush = new SolidBrush(Color.FromArgb(40, 255, 255, 255));
+                SolidBrush shadowBrush = new SolidBrush(Color.FromArgb(40, 0, 0, 0));
+
+
+                StringFormat format = new StringFormat();
+                format.Alignment = StringAlignment.Center;
+                format.FormatFlags = StringFormatFlags.MeasureTrailingSpaces;
+
+                int index = 0;
+                int divisor = 10;
+                float offsetX = 0;
+                float offsetY = 0;
+                SizeF textSize = graphics.MeasureString(message, font);
+
+                int increment = Convert.ToInt32(textSize.Width);
+
+                while(offsetX < image.Width)
+                {
+                    Console.WriteLine("***** New Column *****");
+                    while (offsetY <= image.Height)
+                    {
+                        Console.WriteLine($"offsetX: {offsetX}, offsetY: {offsetY}");
+                        Matrix matrix = new Matrix();
+                        matrix.Translate(offsetX, offsetY);
+                        matrix.Rotate(-45.0f);
+
+                        graphics.Transform = matrix;
+                        string messageText = message;
+                        if (debug)
+                        {
+                            messageText += $" {offsetX} {offsetY}";
+                        }
+                        graphics.DrawString(messageText, font, shadowBrush, 2, 52, format);
+                        graphics.DrawString(messageText, font, brush, 0, 50, format);
+
+                        offsetY += increment;
+                        index++;
+                    }
+                    offsetY = 0;
+                    offsetX += increment;
+                }
+
+
+                
+                string outputFileName = System.IO.Path.GetFileNameWithoutExtension(fileName) + System.IO.Path.GetExtension(fileName) ;
+                image.Save(outputPath + outputFileName);
+            }
+        }
+    }
+}
